6c24e141eb1ef70d02d17dec8d14b069e2296a8f
Luis Alejandro Vera Moreno <98617444+SouthThunder@users.noreply.github.com>
2024-02-18 12:33:33 -0500

    feature: added html template

--- src/main/java/com/example/demo/controller/ApplicationController.java
+++ src/main/java/com/example/demo/controller/ApplicationController.java
@@ -14,27 +14,5 @@ import java.util.List;
 @RequestMapping("/applications")
 public class ApplicationController {
 
-    @Autowired
-    private Repository applicationrepository;
 
-    
-    @PostMapping
-    public ResponseEntity<Application> createApplication(@Valid @RequestBody Application application) {
-        Application createdApplication = applicationService.createApplication(application);
-        return new ResponseEntity<>(createdApplication, HttpStatus.CREATED);
-    }
-
-    // Endpoint para actualizar una aplicación existente
-    @PutMapping("/{id}")
-    public ResponseEntity<Application> updateApplication(@PathVariable Long id, @Valid @RequestBody Application application) {
-        Application updatedApplication = applicationService.updateApplication(id, application);
-        return new ResponseEntity<>(updatedApplication, HttpStatus.OK);
-    }
-
-    // Endpoint para eliminar una aplicación por su ID
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteApplication(@PathVariable Long id) {
-        applicationService.deleteApplication(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
 }
