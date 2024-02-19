f424418a72e6bc03eb78e02f65be1d59ac04d6a9
CarlosLopez <carlosgablopezm@gmail.com>
2024-02-19 09:26:26 -0500

    idk

--- src/main/java/com/example/demo/controller/ApplicationController.java
+++ src/main/java/com/example/demo/controller/ApplicationController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.entity.Application;
@@ -8,8 +8,8 @@ import com.example.demo.repository.Repository;
 
 
 
-@RestController
-@RequestMapping("/applications")
+@Controller
+
 public class ApplicationController {
 
     @Autowired
